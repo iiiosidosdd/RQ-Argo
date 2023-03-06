5f349bd8fc0111a103d21024a2c4003875d1796f
iiiosidosdd <117013595+iiiosidosdd@users.noreply.github.com>
2023-03-06 04:13:56 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const username = process.env.WEB_USERNAME || "admin";
-const password = process.env.WEB_PASSWORD || "password";
+const username = process.env.WEB_USERNAME || "mufgg";
+const password = process.env.WEB_PASSWORD || "MIMA321...mima321...";
 const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
